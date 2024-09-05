75a38212f3d07d89f562170ee013ca52b2babdea
Вадим <vadimkarworkmail@gmail.com>
2024-09-05 18:33:24 +0300

    Added new questions.

--- app/src/main/java/com/justcircleprod/catquiz/core/data/quizConstants/level/LevelPrices.kt
+++ app/src/main/java/com/justcircleprod/catquiz/core/data/quizConstants/level/LevelPrices.kt
@@ -2,10 +2,10 @@ package com.justcircleprod.catquiz.core.data.quizConstants.level
 
 object LevelPrices {
 
-    const val LEVEL_2_PRICE = 500
-    const val LEVEL_3_PRICE = 900
-    const val LEVEL_4_PRICE = 1700
-    const val LEVEL_5_PRICE = 2100
-    const val LEVEL_6_PRICE = 3050
-    const val LEVEL_7_PRICE = 4100
+    const val LEVEL_2_PRICE = 650
+    const val LEVEL_3_PRICE = 1300
+    const val LEVEL_4_PRICE = 2300
+    const val LEVEL_5_PRICE = 2950
+    const val LEVEL_6_PRICE = 4500
+    const val LEVEL_7_PRICE = 6200
 }
